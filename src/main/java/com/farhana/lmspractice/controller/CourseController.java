77c18bcb0099b4a8fcd5496ea2b3302d46fee08a
farhanamomin <102279699+farhanamomin@users.noreply.github.com>
2023-02-22 05:32:05 +0000

    new

--- src/main/java/com/farhana/lmspractice/controller/CourseController.java
+++ src/main/java/com/farhana/lmspractice/controller/CourseController.java
@@ -19,7 +19,7 @@ public class CourseController {
 
     @Autowired
     CourseService courseService;
-      @PostMapping("/welcome")
+      @GetMapping("/welcome")
     public  String welcomeMessage(){
         return "WELCOME";
     }
